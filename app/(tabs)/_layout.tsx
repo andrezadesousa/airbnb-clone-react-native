fc5fd29cad05a282aa2ba53e02ccfcb8d9f77f53
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-12-18 15:24:35 -0300

    Style: add sidebar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import Colors from "@/constants/Colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons"; 
 import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 const Layout = () => {
@@ -38,6 +39,26 @@ const Layout = () => {
           tabBarLabel: "Trips",
           tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="airbnb" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          tabBarLabel: 'Inbox',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="message-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: 'Profile',
+
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
           ),
         }}
       />
