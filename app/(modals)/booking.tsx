023af6426e04554ad1516f9a8aaa0fa48af13062
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-12-19 15:25:35 -0300

    Fix: created navigation

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,12 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router'
 
 const Page = () => {
   return (
     <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
+      <Text>BOOK</Text>
     </View>
   )
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -55,8 +55,6 @@ const Layout = () => {
         name="profile"
         options={{
           tabBarLabel: 'Profile',
-
-          headerShown: false,
           tabBarIcon: ({ size, color }) => (
             <Ionicons name="person-circle-outline" size={size} color={color} />
           ),
